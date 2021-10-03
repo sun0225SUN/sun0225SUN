43e6e69299cdf280e423e5da2bf0bbdfca1017b1
sunshine <sun412926@gmail.com>
2021-10-03 20:45:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ function generateProgressBar() {
 
 const readme = `\
 ### Hi there 👋
+
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
 ---
+
 ⏰ Updated on ${new Date().toUTCString()}
----
+
+![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
+`
+
+console.log(readme)
